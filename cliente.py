5363676275a7cfbbd0ea574e8c01a97458c57aae
Patrick Melo <patrickmelo13@gmail.com>
2022-11-06 18:39:37 -0300

    New Repo
    Commiting the codes I made while learning Python

---
+++ cliente.py
@@ -0,0 +1,12 @@
+class Cliente:
+	def __init__(self, nome, email, plano):
+		self.nome = nome
+		self.email = email
+		lista_planos = ["basic", "premium"]
+		if plano in lista_planos:
+			self.plano = plano
+		else:
+			raise Exception("Plano inválido")
+
+cliente = Cliente("Lira", "lira@gmail.com", "blabla")
+print(cliente)
---
+++ test.py
@@ -0,0 +1,2 @@
+a = [' ' for _ in range(9)]
+print(a)
